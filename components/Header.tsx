acea35e414cee7aaff50132d47f1dc9a34ad03ee
elliot <elliot@norrevik.se>
2025-03-20 15:59:46 +0100

    Updated Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,22 +1,19 @@
 export default function Header() {
   return (
     <div>
-      <h1 className="text-2xl sm:text-3xl font-bold mb-0 leading-tight font-mega">Gemini Co-Drawing</h1>
+      <h1 className="text-2xl sm:text-3xl font-bold mb-0 leading-tight font-mega">Vibe Drawing</h1>
+   
+    
       <p className="text-sm sm:text-base text-gray-500 mt-1">
-        Built with{" "}
-        <a className="underline" href="https://ai.google.dev/gemini-api/docs/image-generation" target="_blank" rel="noopener noreferrer">
-          Gemini 2.0 native image generation
+        A fork of {" "}
+        <a className="underline" href="https://huggingface.co/spaces/Trudy/gemini-codrawing" target="_blank" rel="noopener noreferrer">
+        Gemini Co-Drawing
         </a>
-      </p>
-      <p className="text-sm sm:text-base text-gray-500 mt-1">
-        by{" "}
-        <a className="underline" href="https://x.com/trudypainter" target="_blank" rel="noopener noreferrer">
-          @trudypainter
-        </a>
-        {" "}and{" "}
-        <a className="underline" href="https://x.com/alexanderchen" target="_blank" rel="noopener noreferrer">
-          @alexanderchen
-        </a>
+       {/* <br />
+      
+        <a className="underline" href="https://github.com/elliot-evno/draw-ai" target="_blank" rel="noopener noreferrer">
+        Github
+        </a> */}
       </p>
     </div>
   );
