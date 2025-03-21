198bdd34bef1beb29f5f17ca24e0cbfad4850685
elliot <elliot@norrevik.se>
2025-03-21 16:55:01 +0100

    Added star on github button

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,5 @@
+import Github from "./ui/Github";
+
 interface HeaderProps {
   darkMode: boolean;
 }
@@ -8,18 +10,9 @@ export default function Header({ darkMode }: HeaderProps) {
       <h1 className={`text-2xl sm:text-3xl font-bold mb-0 leading-tight font-mega ${darkMode ? 'text-white' : 'text-black'}`}>
         Vibe Drawing
       </h1>
-      
-      <p className={`text-sm sm:text-base mt-1 ${darkMode ? 'text-gray-400' : 'text-gray-500'}`}>
-        A fork of {" "}
-        <a className="underline" href="https://huggingface.co/spaces/Trudy/gemini-codrawing" target="_blank" rel="noopener noreferrer">
-        Gemini Co-Drawing
-        </a>
-       {/* <br />
-      
-        <a className="underline" href="https://github.com/elliot-evno/draw-ai" target="_blank" rel="noopener noreferrer">
-        Github
-        </a> */}
-      </p>
+      <div className="flex items-center mt-2">
+     <Github onClick={() => window.open('https://github.com/elliot-evno/draw-ai', '_blank')}/>
+    </div>
     </div>
   );
 } 
